b8d54314a134fd203ca5c8304af3ecb90bcb6e51
BellatiS <bellati.samuele@iisgalvanimi.edu.it>
2019-11-28 13:44:20 +0000

    model.pug

--- server-node/index.js
+++ server-node/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 const models = require('./models.json');
 
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'pug'); 
 
 app.listen(3000, function () {
@@ -13,4 +14,12 @@ app.get('/', function (req, res) {
       title: "Scegli il modello",
       models: models.modelli
  });
+});
+
+app.get('/model', (req, res) => {
+  const model = models.modelli.find((m) => m.id === req.query.id);
+  res.render('model', {
+    title: `About ${models.name} ${models.theme} ${models.year}`,
+    model,
+  });
 });
